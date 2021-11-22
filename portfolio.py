a7c6fdec84fe0d8fecd23034a524ea4f7bffffa8
quyiyao <quyiyao1999@gmail.com>
2021-11-21 23:11:43 -0500

    Add an implementation of portfolio

---
+++ portfolio.py
@@ -0,0 +1,12 @@
+
+class portfolio:
+    def __init__(self):
+        self._stocks = []
+
+    def buy(self, name, shares, price):
+        self._stocks.append((name, shares, price))
+    
+    def cost(self):
+        return sum(
+            shares * price for _, shares, price in self._stocks
+        )
